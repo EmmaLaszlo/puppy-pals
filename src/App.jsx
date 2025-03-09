9b1f4e831b692862ccd6aecfa5cb102b39ebdc95
Emma Grace <emmalaslo@gmail.com>
2025-03-09 10:33:45 -0400

    Added map function to display puppy names

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,23 @@
-import './App.css'
+import { useState } from 'react';  // Step 1: Import useState
+import { puppyList } from './data.js';
+import './App.css';
 
 function App() {
-  return <div></div>;
+
+  // Step 2: Use useState to manage the puppyList data
+  const [puppies, setPuppies] = useState(puppyList);
+
+  // Step 3: Log the puppies array
+  console.log(puppies);
+
+  return <div className="App">
+ {
+        puppies.map((puppy) => {
+          return <p key={puppy.id}>{puppy.name}</p>;
+        })
+      }
+
+  </div>;
 }
 
 export default App;
-
